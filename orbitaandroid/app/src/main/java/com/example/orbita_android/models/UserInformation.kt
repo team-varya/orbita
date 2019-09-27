d8b5b606a4e45b107b8e375cf3a9f013248ae435
Koekin Yaroslav <ykoekin@gmail.com>
2019-09-27 20:25:09 +0300

    Добавил комментарии

--- orbitaandroid/app/src/main/java/com/example/orbita_android/models/UserInformation.kt
+++ orbitaandroid/app/src/main/java/com/example/orbita_android/models/UserInformation.kt
@@ -1,5 +1,13 @@
 package com.example.orbita_android.models
 
+
+
+/**
+ * Информация о пользователе.
+ * @param packageCost -- стоимость пачки сигарет.
+ * @param dayCycle -- количество дней
+ * @param packagesPerCycle -- количество пачек сигарет, выкуриваемых пользователем в dayCycle дней
+ */
 class UserInformation (
     packageCost: Int,
     dayCycle: Int,
